fe1e46a47ecea1b045e98038a9cf8975b2014501
maariyadiminsky <mariya.github@gmail.com>
2021-07-27 16:26:55 -0600

    changes results page to a functional component

--- src/pages/ResultsPage.js
+++ src/pages/ResultsPage.js
@@ -4,30 +4,28 @@ import { getLifePathNumber, getAttitudeNumber, getDayNumberFull, getNumerologySi
 
 import "../css/Page.css";
 
-class ResultsPage extends Component {
-    render() {
-        return (
-            <div className="page ui grid">
-                <div className="three column row">
-                    <Result
-                        number={3}
-                        type={"LifePath"}
-                        details={"This means...."}
-                    />
-                    <Result
-                        number={2}
-                        type={"Attitude"}
-                        details={"This means...."}
-                    />
-                    <Result
-                        number={3}
-                        type={"Character"}
-                        details={"This means...."}
-                    />
-                </div>
+const ResultsPage = () => {
+    return (
+        <div className="page ui grid">
+            <div className="three column row">
+                <Result
+                    number={3}
+                    type={"LifePath"}
+                    details={"This means...."}
+                />
+                <Result
+                    number={2}
+                    type={"Attitude"}
+                    details={"This means...."}
+                />
+                <Result
+                    number={3}
+                    type={"Character"}
+                    details={"This means...."}
+                />
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default ResultsPage;
