d2c15ea86dce5921bf438696c41c27251aa47c92
maariyadiminsky <mariya.github@gmail.com>
2021-07-27 11:05:34 -0600

    improved styles for results page

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,19 +2,43 @@ import React from "react";
 
 import "../css/Result.css";
 
+const LifePath = {
+    Header: "Life Path",
+    Sentence: "life path"
+}
+
+const Character = {
+    Header: "Character Number",
+    Sentence: "day/character"
+}
+
+const Attitude = {
+    Header: "Attitude Number",
+    Sentence: "attitude"
+}
+
+const DataType = {
+    LifePath: LifePath,
+    Character: Character,
+    Attitude: Attitude,
+}
+
 const Result = ({ number, type, details }) => {
+    const numberType = type === undefined ? DataType[LifePath] : DataType[type];
+
     return (
-        <div className="result">
+        <div className="result column">
             <div className="result ui card centered">
                 <div className="content">
-                    <h1>Life Path</h1>
+                    <h1>{numberType.Header}</h1>
                     <div className="center aligned description">
-                        <p>You are a number 3 life path.</p>
+                        <p>{`You are a number ${number} ${numberType.Sentence}.`}</p>
+                        <p>{details}</p>
                     </div>
                 </div>
                 <div className="extra content">
                     <div className="center aligned author">
-                        Learn More...
+                        <a href="#">Learn more...</a>
                     </div>
                 </div>
             </div>
--- src/pages/ResultsPage.js
+++ src/pages/ResultsPage.js
@@ -6,9 +6,24 @@ import "../css/Page.css";
 class ResultsPage extends Component {
     render() {
         return (
-            <div className="page">
-                result
-                <Result />
+            <div className="page ui grid">
+                <div className="three column row">
+                    <Result
+                        number={3}
+                        type={"LifePath"}
+                        details={"This means...."}
+                    />
+                    <Result
+                        number={2}
+                        type={"Attitude"}
+                        details={"This means...."}
+                    />
+                    <Result
+                        number={3}
+                        type={"Character"}
+                        details={"This means...."}
+                    />
+                </div>
             </div>
         );
     }
